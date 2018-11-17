2b064c00ea63362831cf7cf3c54eeddc4e38496e
samperid <samperid@bu.edu>
2018-11-17 15:40:07 -0500

    added original test code

--- Image_Skin/test_code.py
+++ Image_Skin/test_code.py
@@ -3,7 +3,7 @@ from neopixel import *
 import argparse
 
 # LED strip configuration:
-LED_COUNT      = 12      # Number of LED pixels.
+LED_COUNT      = 16      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
@@ -17,7 +17,7 @@ strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, 
 # Intialize the library (must be called once before other functions).
 strip.begin()
 
-for i in range(20):
+for i in range(16):
     wait_ms = 50
     strip.setPixelColor(i, Color(255, 255, 0))
     strip.show()
