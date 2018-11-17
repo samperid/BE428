4f04e1d65085b2ca3cf6a913fa41e8347f365d2c
samperid <samperid@bu.edu>
2018-11-17 17:02:16 -0500

    updated analyze_image

--- Image_Skin/analyze_image.py
+++ Image_Skin/analyze_image.py
@@ -1,37 +1,37 @@
-# from PIL import Image
-# import numpy as np
-
-# imag = Image.open("/home/pi/Desktop/BE428/Image_Skin/Images/test0.jpg")
-
-# #Get size of image
-# width,height = imag.size
-
-# pixel_array = np.zeros(((width,height))
-
-# #Convert the image te RGB if it is a .gif for example
-# imag2 = imag.convert('RGB')
-
-# #Get RGB
-# for row in pixel_array:
-#     for column in pixel_array:
-#         pixelRGB = imag2.getpixel((row,column))
-#         R,G,B = pixelRGB
-#         # RGB_vec = [R,G,B]
-#         # pixel_array(row,column) = RGB_vec
-#         #R,G,B = pixelRGB 
-
-# # print(pixel_array)
-
 from PIL import Image
-imag = imag = Image.open("/home/pi/Desktop/BE428/Image_Skin/Images/test0.jpg")
-#Convert the image te RGB if it is a .gif for example
-imag = imag.convert ('RGB')
-#coordinates of the pixel
-X,Y = 0,0
-#Get RGB
-pixelRGB = imag.getpixel((X,Y))
-R,G,B = pixelRGB 
+import numpy as np
 
-print(R)
-print(G)
-print(B)
+imag = Image.open("/home/pi/Desktop/BE428/Image_Skin/Images/test0.jpg")
+
+#Convert the image te RGB if it is a .gif for example
+imag = imag.convert('RGB')
+
+#Get size of image
+width,height = imag.size
+
+pixel_array = np.zeros(((width,height))
+
+#Get RGB
+for row in pixel_array:
+    for column in pixel_array:
+        pixelRGB = imag.getpixel((row,column))
+        R,G,B = pixelRGB
+        # RGB_vec = [R,G,B]
+        # pixel_array(row,column) = RGB_vec
+        #R,G,B = pixelRGB 
+
+# print(pixel_array)
+
+# from PIL import Image
+# imag = imag = Image.open("/home/pi/Desktop/BE428/Image_Skin/Images/test0.jpg")
+# #Convert the image te RGB if it is a .gif for example
+# imag = imag.convert ('RGB')
+# #coordinates of the pixel
+# X,Y = 0,0
+# #Get RGB
+# pixelRGB = imag.getpixel((X,Y))
+# R,G,B = pixelRGB 
+
+# print(R)
+# print(G)
+# print(B)
