0e8711f62b4be77f67da4d94fe37917dd706c839
samperid <samperid@bu.edu>
2018-11-17 13:55:33 -0500

    updated code

--- Image_Skin/acquire_image.py
+++ Image_Skin/acquire_image.py
@@ -3,9 +3,11 @@ from time import sleep
 
 camera = PiCamera()
 
+name = raw_input("Insert name for images: ")
+
 camera.start_preview()
-for i in range(5):
+for i in range(3):
     sleep(5)
-    camera.capture('/home/pi/Desktop//BE428/Image_Skin/Images/image%s.jpg' % i)
+    camera.capture('/home/pi/Desktop//BE428/Image_Skin/Images/%s%s.jpg' % name,i)
 camera.stop_preview()
 
