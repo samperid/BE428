5dfc185c929c112decd3fdd5710b81605f68467b
samperid <samperid@bu.edu>
2018-11-17 17:04:21 -0500

    updated analyze_image

--- Image_Skin/analyze_image.py
+++ Image_Skin/analyze_image.py
@@ -13,15 +13,17 @@ print(height)
 pixel_array = np.zeros(((width,height))
 
 #Get RGB
-for row in range(width):
-    for column in range(height):
-        pixelRGB = imag.getpixel((row,column))
-        R,G,B = pixelRGB
+# for row in range(width):
+#     for column in range(height):
+#         pixelRGB = imag.getpixel((row,column))
+#         R,G,B = pixelRGB
         # RGB_vec = [R,G,B]
         # pixel_array(row,column) = RGB_vec
         #R,G,B = pixelRGB 
 
 # print(pixel_array)
+
+################################################################################################################
 
 # from PIL import Image
 # imag = imag = Image.open("/home/pi/Desktop/BE428/Image_Skin/Images/test0.jpg")
