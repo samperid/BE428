be86bd48adaf7d9b6e06123bd8b9a983ca6df55a
samperid <samperid@bu.edu>
2018-11-17 17:12:51 -0500

    updated analyze_image

--- Image_Skin/analyze_image.py
+++ Image_Skin/analyze_image.py
@@ -4,10 +4,16 @@ imag = imag = Image.open("/home/pi/Desktop/BE428/Image_Skin/Images/test0.jpg")
 #Convert the image te RGB if it is a .gif for example
 imag = imag.convert ('RGB')
 
+#Get image size
 width,height = imag.size
-print(width)
-print(height)
 
+for x in range(width):
+    for y in range(height):
+        pixelRGB = imag.getpixel((x,y))
+        R,G,B = pixelRGB 
+        print(R)
+        print(G)
+        print(B)
 
 #coordinates of the pixel
 X,Y = 0,0
