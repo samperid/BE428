e4aa6e854ad4bc4d1b2c8168d076ba7300bb30b0
samperid <samperid@bu.edu>
2018-11-17 13:56:44 -0500

    updated code

--- Image_Skin/acquire_image.py
+++ Image_Skin/acquire_image.py
@@ -8,6 +8,7 @@ name = raw_input("Insert name for images: ")
 camera.start_preview()
 for i in range(3):
     sleep(5)
-    camera.capture('/home/pi/Desktop//BE428/Image_Skin/Images/%s%s.jpg' % name,i)
+    full_str = name+i
+    camera.capture('/home/pi/Desktop//BE428/Image_Skin/Images/%s.jpg' % full_str)
 camera.stop_preview()
 
