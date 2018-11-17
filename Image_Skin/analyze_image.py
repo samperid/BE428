89faad93911055d90c1dd3ddf1b50cbce9fa7e4a
samperid <samperid@bu.edu>
2018-11-17 17:08:06 -0500

    updated analyze_image

--- Image_Skin/analyze_image.py
+++ Image_Skin/analyze_image.py
@@ -3,6 +3,10 @@ from PIL import Image
 imag = imag = Image.open("/home/pi/Desktop/BE428/Image_Skin/Images/test0.jpg")
 #Convert the image te RGB if it is a .gif for example
 imag = imag.convert ('RGB')
+
+value = imag.size
+print(value)
+
 #coordinates of the pixel
 X,Y = 0,0
 #Get RGB
