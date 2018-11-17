9dc7da6381ca10325313c11852e4f9b08c72182c
samperid <samperid@bu.edu>
2018-11-17 16:53:36 -0500

    updated analyze_image

--- Image_Skin/analyze_image.py
+++ Image_Skin/analyze_image.py
@@ -6,7 +6,7 @@ imag = Image.open("/home/pi/Desktop/BE428/Image_Skin/Images/test0.jpg")
 #Get size of image
 width,height = imag.size
 
-pixel_array = np.zeros(width,height)
+pixel_array = np.zeros(((width,height))
 
 #Convert the image te RGB if it is a .gif for example
 imag = imag.convert ('RGB')
