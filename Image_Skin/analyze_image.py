6ebae7fa775eca7f0e8af3750802974ef33c2dd7
samperid <samperid@bu.edu>
2018-11-17 17:00:36 -0500

    updated analyze_image

--- Image_Skin/analyze_image.py
+++ Image_Skin/analyze_image.py
@@ -22,7 +22,7 @@
 
 # # print(pixel_array)
 
-import Image
+from PIL import Image
 imag = imag = Image.open("/home/pi/Desktop/BE428/Image_Skin/Images/test0.jpg")
 #Convert the image te RGB if it is a .gif for example
 imag = imag.convert ('RGB')
