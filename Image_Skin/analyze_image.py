6493e8913270cf157f922060de5a7a11fa30cb0e
samperid <samperid@bu.edu>
2018-11-18 16:50:56 -0500

    updated changes for demo

--- Image_Skin/analyze_image.py
+++ Image_Skin/analyze_image.py
@@ -34,11 +34,13 @@ for x in range(width):
 total_R = 0
 total_G = 0
 total_B = 0
+total_bright = 0
 for x in range(width):
     for y in range(height):
         total_R = total_R + pixel_dict[x][y].red
         total_G = total_G + pixel_dict[x][y].green
         total_B = total_B + pixel_dict[x][y].blue
+        total_bright = total_bright + pixel_dict[x][y].brightness
         '''
         print("Pixel %i,%i:" % (x,y))
         print("\n")
@@ -53,7 +55,11 @@ for x in range(width):
 avg_R = total_R / (width*height)
 avg_G = total_G / (width*height)
 avg_B = total_B / (width*height)
-
+avg_bright = total_bright / (width*height)
+print("\n")
+print("Image File: DR3_R")
 print("avg_R = %f" % avg_R)
 print("avg_G = %f" % avg_G)
 print("avg_B = %f" % avg_B)
+print("avg_brightness = %f" % avg_bright)
+print("\n")
