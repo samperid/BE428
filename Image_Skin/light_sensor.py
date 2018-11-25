f74601a7c5730c0b3846b6d003b05d9eb088ac85
samperid <samperid@bu.edu>
2018-11-25 15:41:33 -0500

    added code for sensor

---
+++ Image_Skin/light_sensor.py
@@ -0,0 +1,10 @@
+import board
+import busio
+import adafruit_tsl2561
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_tsl2561.TSL2561(i2c)
+
+print('Lux: {}'.format(sensor.lux))
+print('Broadband: {}'.format(sensor.broadband))
+print('Infrared: {}'.format(sensor.infrared))
+print('Luminosity: {}'.format(sensor.luminosity))
