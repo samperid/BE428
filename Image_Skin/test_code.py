94678bed7a18d412ab4d17210dca8cfa5cb63c63
samperid <samperid@bu.edu>
2018-11-29 00:16:40 -0500

    updated test_code.py

--- Image_Skin/test_code.py
+++ Image_Skin/test_code.py
@@ -8,7 +8,7 @@ LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 100     # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
@@ -17,13 +17,9 @@ strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, 
 # Intialize the library (must be called once before other functions).
 strip.begin()
 
+#Green 
 for i in range(12):
     wait_ms = 50
-    #Color(255,0,0) = Blue & Color(0,0,255) = Green
-    # if i % 2 == 0 and i % 4 != 0:
-    #     strip.setPixelColor(i, Color(0, 0, 255))
-    #     strip.show()
-    #     time.sleep(wait_ms/1000.0)
     strip.setPixelColor(i,Color(255,0,0))
     strip.show()
     time.sleep(wait_ms/1000.0)
@@ -32,11 +28,23 @@ time.sleep(5)
 
 for i in range(12):
     wait_ms = 50
-    #Color(255,0,0) = Blue & Color(0,0,255) = Green
-    # if i % 2 == 0 and i % 4 != 0:
-    #     strip.setPixelColor(i, Color(0, 0, 255))
-    #     strip.show()
-    #     time.sleep(wait_ms/1000.0)
+    strip.setPixelColor(i,Color(0,255,0))
+    strip.show()
+    time.sleep(wait_ms/1000.0)
+
+time.sleep(5)
+
+for i in range(12):
+    wait_ms = 50
+    strip.setPixelColor(i,Color(0,0,255))
+    strip.show()
+    time.sleep(wait_ms/1000.0)
+
+time.sleep(5)
+
+#Turn Off
+for i in range(12):
+    wait_ms = 50
     strip.setPixelColor(i,Color(0,0,0))
     strip.show()
     time.sleep(wait_ms/1000.0)
